9082cb593c5d79113e292afa3ea9e397f20a0ca3
OA <OA@gl.com>
2022-11-02 20:33:01 +0200

    Unit2_C_Programming/

---
+++ Unit2_C_Programming/lesson_3_C_Basics/Homework_1/Ex2 - Write C Program to Print a Integer Entered by a User/src/test.c
@@ -0,0 +1,67 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void flush(void);
+
+int main(void) {
+
+	int ROWS = 6;
+	int COLS = 26;
+	int i;
+	int j;
+	for(i = 0; i <= ROWS; ++i)
+	{	if(i == 1){
+			printf("C Programming\n");
+			continue;
+		}
+		if(i == 3){
+			/*code here*/
+
+			int num;
+			printf("Enter an integer: ");
+			flush();
+			scanf("%d", &num);
+			printf("You entered: %d", num);
+			flush();
+
+			printf("\n");
+			continue;
+		}
+
+		for(j = 0; j <= COLS; ++j)
+		{
+			if( (i == 0) && (j >= 10 && j<24))
+			{
+				if(j == 10){
+					printf("Console-output");
+				}
+				continue;
+			}
+				else{
+					printf("#");
+				}
+		}
+		if(i < 4){
+		printf("\n");
+		}
+	}
+	printf("\n");
+
+	return EXIT_SUCCESS;
+}
+
+void flush(void)
+{
+	/*fflush(stdin);*/
+	fflush(stdout);
+}
